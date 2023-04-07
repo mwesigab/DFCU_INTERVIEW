27e5e2372a691aa3eaf75aebf1ab7c30961fd610
Mwesiga <mwesigab@gmail.com>
2023-04-07 15:57:58 +0300

    'init'

--- src/main/java/com/ben/interview/controllers/RequestLoggerController.java
+++ src/main/java/com/ben/interview/controllers/RequestLoggerController.java
@@ -4,6 +4,7 @@ import com.ben.interview.helpers.GenericResponse;
 import com.ben.interview.models.RequestLog;
 import com.ben.interview.services.RequestLoggerService;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ public class RequestLoggerController{
     }
 
     @GetMapping("logs")
+    @CrossOrigin(origins = "http://localhost:3000")
     public GenericResponse getLogs(){
         try {
             Iterable<RequestLog> list = requestLoggerService.getLogs();

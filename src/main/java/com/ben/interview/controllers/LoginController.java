2c54e5427a87cb578daee16da2975910ca2826dc
Mwesiga <mwesigab@gmail.com>
2023-04-07 15:26:26 +0300

    'init'

--- src/main/java/com/ben/interview/controllers/LoginController.java
+++ src/main/java/com/ben/interview/controllers/LoginController.java
@@ -27,7 +27,7 @@ public class LoginController {
         try {
             users=loginService.login(user.getUsername(),user.getPassword());
             if(users.size()==0) return new GenericResponse(HttpStatus.NOT_FOUND.value(), "The user with the specified credentials does not exist.");
-           return new GenericResponse(HttpStatus.OK.value(), "Success", users.get(0));
+           return new GenericResponse(HttpStatus.OK.value(), "Logged In", users.get(0));
         }catch (Exception e){
             e.printStackTrace();
             return new GenericResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage());
@@ -40,7 +40,7 @@ public class LoginController {
         Integer response;
         try {
             response= loginService.logout(user.getUsername(), user.getPassword());
-            if(response > 0) return new GenericResponse(HttpStatus.OK.value(), "Success", response);
+            if(response > 0) return new GenericResponse(HttpStatus.OK.value(), "Logged Out", response);
             else return new GenericResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "Could not log out...");
         }catch (Exception e){
             e.printStackTrace();
